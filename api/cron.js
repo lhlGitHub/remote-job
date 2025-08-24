0cc00c75b216f1615ad24ff64125072ee4763ac4
hailey <312521898@qq.com>
2025-08-24 13:26:14 +0800

    fix: 添加vercel 配置

--- api/cron.js
+++ api/cron.js
@@ -1,6 +1,10 @@
 import { main } from "../index.js";
 
 export default async function handler(req, res) {
+  if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).end("Unauthorized");
+  }
+
   try {
     const newJobs = await main();
     res.status(200).json({ success: true, newJobsCount: newJobs.length });
