44ac684a612db9391fce1e3567b1e499299e43c0
hailey <312521898@qq.com>
2025-08-24 15:29:26 +0800

    fix: 更新puppeteerCore

--- utils/crawler.js
+++ utils/crawler.js
@@ -1,23 +1,27 @@
 const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
 const puppeteerCore = require("puppeteer-core");
 
 async function launchBrowser() {
-  const isLocal = process.env.NODE_ENV !== "production";
+  const isLocal =
+    process.env.NODE_ENV !== "production" &&
+    !process.env.AWS_LAMBDA_FUNCTION_NAME;
 
   if (isLocal) {
-    const puppeteer = require("puppeteer");
+    // 本地环境
     return await puppeteer.launch({
       executablePath:
         "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
       headless: false,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
       defaultViewport: null,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
   } else {
+    // Vercel 生产环境
     return await puppeteerCore.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(), // ⚠️ 注意这里要加 ()
+      executablePath: chromium.executablePath, // ✅ 不要 await
       headless: chromium.headless,
     });
   }
