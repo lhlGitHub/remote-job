6497c25ed87389270d96a9727ae95dd91d2276c2
hailey <312521898@qq.com>
2025-08-26 14:58:57 +0800

    fix: 修复puppeteer 配置

--- utils/crawler.js
+++ utils/crawler.js
@@ -1,48 +1,15 @@
 async function launchBrowser() {
-  const isLocal =
-    process.env.NODE_ENV !== "production" &&
-    !process.env.AWS_LAMBDA_FUNCTION_NAME;
+  const IS_LOCAL = process.env.LOCAL === "true";
+  const browser = await puppeteer.launch({
+    executablePath: IS_LOCAL
+      ? "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+      : undefined,
+    headless: IS_LOCAL ? false : "new",
+    defaultViewport: null,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
-  if (isLocal) {
-    // 本地环境
-    const puppeteer = require("puppeteer");
-    return await puppeteer.launch({
-      executablePath:
-        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-      headless: false,
-      defaultViewport: null,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-  } else {
-    // 生产环境 (Vercel)
-
-    const chromium = require("@sparticuz/chromium");
-    const puppeteerCore = require("puppeteer-core");
-
-    // 确保 Chromium 已准备就绪
-    await chromium.font(
-      "https://raw.githubusercontent.com/googlefonts/noto-cjk/main/Sans/OTF/SimplifiedChinese/NotoSansCJKsc-Regular.otf"
-    );
-
-    const execPath = await chromium.executablePath();
-    return await puppeteerCore.launch({
-      args: [
-        ...chromium.args,
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-accelerated-2d-canvas",
-        "--no-first-run",
-        "--no-zygote",
-        "--disable-gpu",
-        "--single-process",
-      ],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: execPath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
-  }
+  return browser;
 }
 
 module.exports = { launchBrowser };
