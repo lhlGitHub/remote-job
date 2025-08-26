56f6888ec49225f5a2b58cafe14eadb21339d577
hailey <312521898@qq.com>
2025-08-26 15:23:03 +0800

    fix: ud

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,9 @@ module.exports = {
       name: "remote-job",
       script: "./index.js",
       interpreter: "/usr/bin/node",
-      autorestart: true, // ✅ 保持进程存活
+      autorestart: false, // ✅ 执行完退出，不循环
       watch: false,
-      cron_restart: "0 */4 * * *", // 每 4 小时重启一次
+      cron_restart: "0 */4 * * *", // 每 4 小时执行一次
       env: {
         NODE_ENV: "production",
         LOCAL: "false",
